538f43b28d49e8e1d8d87b54474168a134eb1bb8
D <me@kumo.dev>
2025-02-01 19:39:36 +1100

    feat: make now() an observable signal

--- src/base/time.ts
+++ src/base/time.ts
@@ -1,10 +1,28 @@
 import { TZDate } from '@date-fns/tz';
 import { format } from 'date-fns';
+import { Signal } from 'signal';
+
+const timeStore = new Signal.State({
+  currentTime: new TZDate(new Date(), 'Australia/Sydney'),
+});
+let updaterInterval: NodeJS.Timeout | undefined;
+function setupTimeUpdater() {
+  if (updaterInterval) {
+    return;
+  }
+  // Update the time store every minute
+  updaterInterval = setInterval(() => {
+    timeStore.set({
+      currentTime: new TZDate(new Date(), 'Australia/Sydney'),
+    });
+  }, 60 * 1000);
+}
+setupTimeUpdater();
 
 export function time(d: TZDate) {
   return format(d, 'HHmm');
 }
 
 export function now() {
-  return new TZDate(new Date(), 'Australia/Sydney');
+  return timeStore.get().currentTime;
 }
